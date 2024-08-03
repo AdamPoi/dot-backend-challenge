ce872a57360459e1e90d869ebbf793795ef75d5e
AdamPoi <isadamrr@gmail.com>
2024-08-03 20:20:38 +0700

    feat: add swagger library

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +23,17 @@ async function bootstrap() {
     }),
   );
   const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle(configService.get('APP_NAME'))
+    .setDescription(configService.get('APP_DESCRIPTION'))
+    .setVersion(configService.get('APP_VERSION'))
+    .addBearerAuth()
+    .addBasicAuth({ type: 'apiKey', name: 'accessToken', in: 'query' })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const port = configService.get('APP_PORT');
   await app.listen(port);
 }
