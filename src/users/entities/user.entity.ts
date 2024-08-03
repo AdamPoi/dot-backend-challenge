947519969e5194e5fef1be2194d39df45bb4be47
AdamPoi <isadamrr@gmail.com>
2024-08-03 16:02:41 +0700

    feat: add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @Exclude()
+  @Column()
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
