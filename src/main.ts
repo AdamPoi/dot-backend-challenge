363d88123572ade82fef7617ead4c22ea3c49071
AdamPoi <isadamrr@gmail.com>
2024-08-03 15:59:23 +0700

    feat: add helmet & enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(helmet());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+  const configService = app.get(ConfigService);
+  const port = configService.get('APP_PORT');
+  await app.listen(port);
 }
 bootstrap();
