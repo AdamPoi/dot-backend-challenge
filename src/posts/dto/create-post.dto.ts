e7834f495940fc5a064fa6faceb751057b7c7b22
AdamPoi <isadamrr@gmail.com>
2024-08-03 23:00:41 +0700

    feat(post): integrate to swagger

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreatePostDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   title: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   content: string;
 }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -16,36 +16,54 @@ import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { JWTAuthGuard } from 'src/auth/jwt-auth.guard';
 import { User } from 'src/users/entities/user.entity';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(JWTAuthGuard)
+@ApiTags('posts')
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: Post })
+  @ApiBody({ type: CreatePostDto })
   create(@Body() createPostDto: CreatePostDto, @Req() req: { user: User }) {
     return this.postsService.create(createPostDto, req.user);
   }
   @Get()
+  @ApiOkResponse({ type: [Post] })
   findAll() {
     return this.postsService.findAll();
   }
+
   @Get('users/:userId')
+  @ApiOkResponse({ type: [Post] })
   findAllByUser(@Param('userId') userId: number) {
     return this.postsService.findAllByUser(userId);
   }
+
   @Get(':id')
+  @ApiOkResponse({ type: Post })
+  @ApiNotFoundResponse({ description: 'Post not found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized access to resource' })
   findOne(@Param('id') id: number, @Req() req: { user: User }) {
     return this.postsService.findOne(id, req.user);
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.postsService.findOne(+id);
-  // }
-
   @Patch(':id')
+  @ApiOkResponse({ type: Post })
+  @ApiNotFoundResponse({ description: 'Post not found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized access to resource' })
+  @ApiBadRequestResponse()
   update(
     @Param('id') id: string,
     @Body() updatePostDto: UpdatePostDto,
@@ -55,6 +73,9 @@ export class PostsController {
   }
 
   @Delete(':id')
+  @ApiOkResponse({ status: 204 })
+  @ApiNotFoundResponse({ description: 'Post not found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized access to resource' })
   remove(@Param('id') id: string, @Req() req: { user: User }) {
     return this.postsService.remove(+id, req.user);
   }
