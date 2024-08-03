0294703f648b14b1c502e4538454417a5e013a2f
AdamPoi <isadamrr@gmail.com>
2024-08-03 17:17:11 +0700

    feat(auth): add auth function

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { User } from 'src/users/models/user.model';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  getAccessToken(user: User): string {
+    return this.jwtService.sign({ sub: user.id, username: user.username });
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async register(createUserDto: CreateUserDto) {
+    const user = await this.usersService.create(createUserDto);
+    return {
+      user,
+      accessToken: this.getAccessToken(user),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+  async login(loginDto: LoginDto) {
+    const user = await this.usersService.findOneByField(
+      loginDto.username,
+      'username',
+    );
+    if (!user)
+      throw new NotAcceptableException('Incorrect username and password');
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+    const bcrypt = require('bcrypt');
+    if (!(await bcrypt.compare(loginDto.password, user.password)))
+      throw new NotAcceptableException('Incorrect username and password');
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    return {
+      user,
+      accessToken: this.getAccessToken(user),
+    };
   }
 }
