452bee291c97f898b42f617fc88d3e83289bd89a
AdamPoi <isadamrr@gmail.com>
2024-08-03 22:50:33 +0700

    feat(user): integrate to swagger

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class User {
+  @ApiProperty()
   id: number;
+  @ApiProperty()
   username: string;
+  @ApiProperty()
   createdAt: Date;
+  @ApiProperty()
   updatedAt: Date;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,32 +12,52 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  ApiBasicAuth,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 @UseInterceptors(ClassSerializerInterceptor)
+@ApiBasicAuth()
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+  })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOkResponse({
+    description: 'The records have been successfully retrieved.',
+  })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ description: 'The record has been successfully retrieved.' })
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ description: 'The record has been successfully updated.' })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiNoContentResponse({
+    description: 'The record has been successfully deleted.',
+  })
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
